03757abba36f759c7132a91cfaa84e24dec9418e
DHyperYT <79223072+DHyperYT@users.noreply.github.com>
2021-03-23 10:18:43 +0200

    Add files via upload

---
+++ trolling.bat
@@ -0,0 +1,93 @@
+@echo off
+title Le Troll Batch
+
+:start
+echo Choose one
+echo -----------------------------
+echo 1. Troll Prank
+echo 2. Pick up the earth
+echo 3. iFly
+echo -----------------------------
+set /p troll=
+
+if "%troll%" == "1" goto prank
+if "%troll%" == "2" goto earth
+if "%troll%" == "3" goto ifly
+
+cls
+goto start
+
+:prank
+cls
+echo You will need:
+timeout /t 2 /nobreak >Nul
+cls
+echo Some Mentos
+timeout /t 2 /nobreak >Nul
+cls
+echo Coke
+timeout /t 2 /nobreak >Nul
+cls
+echo Ice cube tray
+timeout /t 2 /nobreak >Nul
+cls
+echo How to do it:
+timeout /t 3 /nobreak >Nul
+cls
+echo (*le troll face) Step 1: Fill the ice cube tray with mentos.
+timeout /t 2 /nobreak >Nul
+cls
+echo Step 2: Fill with water and put into fridge.
+timeout /t 2 /nobreak >Nul
+cls
+echo Step 3: Put the ice cubes into the coke glass.
+timeout /t 2 /nobreak >Nul
+cls
+echo Time to...
+timeout /t 1 /nobreak >Nul
+cls
+echo TROLL!
+timeout /t 2 /nobreak >Nul
+cls
+echo Le you: Hey dude want some coke?
+timeout /t 2 /nobreak >Nul
+cls
+echo Le Friend: Yeah, why not.
+timeout /t 2 /nobreak >Nul
+cls
+echo The ice will melt
+timeout /t 2 /nobreak >Nul
+cls
+echo AND BOOM! 
+timeout /t 2 /nobreak >Nul
+cls
+goto start
+
+:earth
+cls
+echo Step 1: Go to Le Antarctica
+timeout /t 2 /nobreak >Nul
+cls
+echo Step 2: Stand on your hands
+timeout /t 2 /nobreak >Nul
+cls
+echo Problem?
+timeout /t 5 /nobreak >Nul
+cls
+goto start
+
+:ifly
+cls
+echo Step 1: Open Le Phone
+timeout /t 2 /nobreak >Nul
+cls
+echo Step 2: Turn on airplane mode
+timeout /t 2 /nobreak >Nul
+cls
+echo Step 3: Hold the phone and jump from le house.
+timeout /t 3 /nobreak >Nul
+cls
+echo Problem Airports?
+timeout /t 5 /nobreak >Nul
+cls
+goto start
